b234e3c587ee4c580a803d2377794fdccb7444b9
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-10 18:06:45 -0500

    todo list program

---
+++ to_do_list.rb
@@ -0,0 +1,212 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+puts todo1
+puts todo2
+puts todo3
+
+todo1.done!
+
+puts todo1
+puts todo2
+puts todo3
+
+# [X] Buy milk
+# [ ] Clean room
+# [ ] Go to gym
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  # rest of class needs implementation
+  def add(chore)
+    raise TypeError, 'can only add Todo objects' unless chore.class == Todo
+
+    @todos << chore
+  end
+  alias_method :<<, :add
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    p @todos.delete_at(index)
+  end
+
+  def to_s
+    "---- Today's Todos ----\n" + @todos.map(&:to_s).join("\n")
+  end
+
+  def to_a
+    @todos
+  end
+end
+
+# given
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+# ---- Adding to the list -----
+
+# add
+list.add(todo1)                 # adds todo1 to end of list, returns list
+list.add(todo2)                 # adds todo2 to end of list, returns list
+list.add(todo3)                 # adds todo3 to end of list, returns list
+# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+
+# <<
+# same behavior as add
+
+# ---- Interrogating the list -----
+
+# size
+p list.size                       # returns 3
+
+# first
+p list.first                      # returns todo1, which is the first item in the list
+
+# last
+p list.last                       # returns todo3, which is the last item in the list
+
+# ---- Retrieving an item in the list ----
+
+# item_at
+#list.item_at                    # raises ArgumentError
+p list.item_at(1)                 # returns 2nd item in list (zero based index)
+#list.item_at(100)               # raises IndexError
+
+# ---- Marking items in the list -----
+
+# mark_done_at
+# list.mark_done_at               # raises ArgumentError
+p list.mark_done_at(1)            # marks the 2nd item as done
+#p list.mark_done_at(100)          # raises IndexError
+
+# mark_undone_at
+# list.mark_undone_at             # raises ArgumentError
+p list.mark_undone_at(0)          # marks the 2nd item as not done,
+# list.mark_undone_at(100)        # raises IndexError
+
+# ---- Deleting from the the list -----
+
+# shift
+# p list.shift                      # removes and returns the first item in list
+
+# pop
+# p list.pop                        # removes and returns the last item in list
+p list
+# remove_at
+# p list.remove_at                  # raises ArgumentError
+# p list.remove_at(1)               # removes and returns the 2nd item
+# list.remove_at(100)             # raises IndexError
+p list
+# ---- Outputting the list -----
+
+# to_s
+# p list.to_s                      # returns string representation of the list
+puts list
+
+# ---- Today's Todos ----
+# [ ] Buy milk
+# [ ] Clean room
+# [ ] Go to gym
+
+# or, if any todos are done
+
+# ---- Today's Todos ----
+# [ ] Buy milk
+# [X] Clean room
+# [ ] Go to gym
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+puts list
+
+list.pop
+
+puts list
+
+list.mark_done_at(1)
+
+puts list
---
+++ to_do_list_with_each.rb
@@ -0,0 +1,137 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+puts todo1
+puts todo2
+puts todo3
+
+todo1.done!
+
+puts todo1
+puts todo2
+puts todo3
+
+# [X] Buy milk
+# [ ] Clean room
+# [ ] Go to gym
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  # rest of class needs implementation
+  def add(chore)
+    raise TypeError, 'can only add Todo objects' unless chore.class == Todo
+
+    @todos << chore
+  end
+  alias_method :<<, :add
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    p @todos.delete_at(index)
+  end
+
+  def to_s
+    "---- Today's Todos ----\n" + @todos.map(&:to_s).join("\n")
+  end
+
+  def to_a
+    @todos
+  end
+
+  def each
+    counter = 0
+
+    while counter < @todo.size
+      
+    end
+  end
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+list.each do |todo|
+  puts todo                   # calls Todo#to_s
+end
+
