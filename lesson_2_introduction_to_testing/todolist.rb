b57047098dffa4f106de3bc786515144fcde0ff6
Fernanda Franco <francodias.fernanda@gmail.com>
2017-02-14 13:47:06 -0500

    started test exercise

---
+++ lesson_2_introduction_to_testing/todolist.rb
@@ -0,0 +1,160 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  # rest of class needs implementation
+  def add(chore)
+    raise TypeError, 'can only add Todo objects' unless chore.instance_of? Todo
+
+    @todos << chore
+  end
+  alias_method :<<, :add
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    p @todos.delete_at(index)
+  end
+
+  def to_s
+    "---- #{title} ----\n" + @todos.map(&:to_s).join("\n")
+  end
+
+  def to_a
+    @todos
+  end
+
+  def each
+    counter = 0
+
+    while counter < @todos.size
+      yield(@todos[counter])
+
+      counter += 1
+    end
+
+    self
+  end
+
+  # def select
+  #   @todos.select { |todo| yield(todo)}
+  # end
+
+  def select
+    selected_list = TodoList.new(title)
+    each do |todo|
+      selected_list << todo if yield(todo)
+    end
+
+    selected_list
+  end
+
+  def find_by_title(string)
+    select { |todo| todo.title == string }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(string)
+    find_by_title(string) && find_by_title(string).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
+end
+
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+puts list
+
+results = list.select { |todo| todo.done? }    # you need to implement this method
+
+puts results.inspect
